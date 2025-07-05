f8ec6235a889c557d7a6f342ab3031438a985708
devang5114 <dthakor5114@gmail.com>
2025-07-05 18:10:58 +0530

    onboarding added in app routes

--- lib/features/splash/view/splash_screen.dart
+++ lib/features/splash/view/splash_screen.dart
@@ -20,7 +20,7 @@ class _SplashScreenState extends State<SplashScreen> {
       if (currentRoute == '/notfound') return;
 
       Future.delayed(Duration(seconds: 1), () {
-        Get.offNamed(AppRouter.dashboardScreen); // or dashboard
+        Get.offNamed(AppRouter.onboarding); // or dashboard
       });
     });
   }
